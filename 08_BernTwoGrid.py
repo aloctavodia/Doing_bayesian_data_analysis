22e5b85d771d50290a12abadf4e7646aef8d8c3d
aloctavodia <aloctavodia@gmail.com>
2014-07-08 21:11:11 -0300

    new and update

---
+++ 08_BernTwoGrid.py
@@ -0,0 +1,108 @@
+from __future__ import division
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+from scipy.stats import beta
+import numpy as np
+
+
+# Specify the grid on theta1,theta2 parameter space.
+n_int = 500 # arbitrary number of intervals for grid on theta.
+theta1 = np.linspace(0, 1, n_int)
+theta2 = theta1
+
+theta1_grid, theta2_grid = np.meshgrid(theta1, theta2)
+
+# Specify the prior probability _masses_ on the grid.
+prior_name = ("Beta","Ripples","Null","Alt")[0] # or define your own.
+if prior_name == "Beta":
+    a1, b1, a2, b2 = 3, 3, 3, 3
+    prior1 = beta.pdf(theta1_grid, a1, b1)
+    prior2 = beta.pdf(theta2_grid, a1, b1)
+    prior = prior1 * prior2
+    prior = prior / np.sum(prior)
+
+if prior_name == "Ripples":
+    m1, m2, k = 0, 1, 0.75 * np.pi
+    prior = np.sin((k*(theta1_grid-m1))**2 + (k*(theta2_grid-m2))**2)**2
+    prior = prior / np.sum(prior)
+
+if prior_name == "Null":
+    # 1's at theta1=theta2, 0's everywhere else:
+    prior = np.eye(len(theta1_grid), len(theta2_grid))
+    prior = prior / np.sum(prior)
+
+if prior_name == "Alt":
+#    # Uniform:
+    prior = np.ones((len(theta1_grid), len(theta2_grid)))
+    prior = prior / np.sum(prior)
+
+# Specify likelihood
+z1, N1, z2, N2 = 5, 7, 2, 7  # data are specified here
+likelihood = theta1_grid**z1 * (1-theta1_grid)**(N1-z1) * theta2_grid**z2 * (1-theta2_grid)**(N2-z2)
+
+# Compute posterior from point-by-point multiplication and normalizing:
+p_data = np.sum(prior * likelihood)
+posterior = (prior * likelihood) / p_data
+
+# Specify the probability mass for the HDI region
+credib = .95
+thin = 4
+color = 'skyblue'
+
+fig = plt.figure(figsize=(12,12))
+
+# prior
+ax = fig.add_subplot(3, 2, 1, projection='3d')
+ax.plot_surface(theta1_grid[::thin,::thin], theta2_grid[::thin,::thin], prior[::thin,::thin], color=color)
+ax.set_xlabel(r'$\theta1$')
+ax.set_ylabel(r'$\theta1$')
+ax.set_zlabel(r'$p(t1,t2)$')
+ax.set_xticklabels([])
+ax.set_yticklabels([])
+ax.set_zticklabels([])
+
+plt.subplot(3, 2, 2)
+plt.contour(theta1_grid, theta2_grid, prior, colors=color)
+plt.xlabel(r'$\theta1$')
+plt.ylabel(r'$\theta1$')
+
+# likelihood
+ax = fig.add_subplot(3, 2, 3, projection='3d')
+ax.plot_surface(theta1_grid[::thin,::thin], theta2_grid[::thin,::thin], likelihood[::thin,::thin], color=color)
+ax.set_xlabel(r'$\theta1$')
+ax.set_ylabel(r'$\theta1$')
+ax.set_zlabel(r'$p(D|t1,t2)$')
+ax.set_xticklabels([])
+ax.set_yticklabels([])
+ax.set_zticklabels([])
+
+plt.subplot(3, 2, 4)
+plt.contour(theta1_grid, theta2_grid, likelihood, colors=color)
+plt.xlabel(r'$\theta1$')
+plt.ylabel(r'$\theta1$')
+plt.plot(0, label='z1,N1,z2,N2=%s,%s,%s,%s' % (z1, N1, z2, N2), alpha=0)
+plt.legend(loc='upper left')
+
+# posterior
+ax = fig.add_subplot(3, 2, 5, projection='3d')
+ax.plot_surface(theta1_grid[::thin,::thin], theta2_grid[::thin,::thin],posterior[::thin,::thin], color=color)
+ax.set_xlabel(r'$\theta1$')
+ax.set_ylabel(r'$\theta1$')
+ax.set_zlabel(r'$p(t1,t2|D)$')
+ax.set_xticklabels([])
+ax.set_yticklabels([])
+ax.set_zticklabels([])
+
+plt.subplot(3, 2, 6)
+plt.contour(theta1_grid, theta2_grid, posterior, colors=color)
+plt.xlabel(r'$\theta1$')
+plt.ylabel(r'$\theta1$')
+plt.plot(0, label='p(D) = %.3e' % p_data, alpha=0)
+plt.legend(loc='upper left')
+# Mark the highest posterior density region
+# TODO 
+
+
+plt.tight_layout()
+plt.savefig('BernTwoGrid_%s.png' % prior_name)
+plt.show()
---
+++ 08_BernTwoPyMC.py
@@ -0,0 +1,70 @@
+from __future__ import division
+import matplotlib.pyplot as plt
+import numpy as np
+import pymc as pm
+from plot_post import plot_post
+
+# Generate the data
+y1 = np.array([1, 1, 1, 1, 1, 0, 0])  # 5 heads and 2 tails
+y2 = np.array([1, 1, 0, 0, 0, 0, 0])  # 2 heads and 5 tails
+
+
+with pm.Model() as model:
+    # define the prior
+    theta1 = pm.Beta('theta1', 3, 3)  # prior
+    theta2 = pm.Beta('theta2', 3, 3)  # prior
+    # define the likelihood
+    y1 = pm.Bernoulli('y1', p=theta1, observed=y1)
+    y2 = pm.Bernoulli('y2', p=theta2, observed=y2)
+
+    # Generate a MCMC chain
+    start = pm.find_MAP()  # Find starting value by optimization
+    trace = pm.sample(10000, pm.Metropolis(),
+                      progressbar=False)  # Use Metropolis sampling
+#    trace = pm.sample(10000, pm.NUTS(), progressbar=False) # Use NUTS sampling
+
+# create an array with the posterior sample
+theta1_sample = trace['theta1']
+theta2_sample = trace['theta2']
+
+# Plot the trajectory of the last 500 sampled values.
+plt.plot(theta1_sample[:-500], theta2_sample[:-500], marker='o')
+plt.xlim(0, 1)
+plt.ylim(0, 1)
+plt.xlabel(r'$\theta1$')
+plt.ylabel(r'$\theta2$')
+
+# Display means in plot.
+plt.plot(0, label='M = %.3f, %.3f' % (np.mean(theta1_sample), np.mean(theta2_sample)), alpha=0.0)
+
+plt.legend(loc='upper left')
+plt.savefig('Figure_8.6.png')
+
+# Plot a histogram of the posterior differences of theta values.
+plt.figure()
+theta_diff = theta1_sample - theta2_sample
+plot_post(theta_diff, xlab=r'\theta1-theta2', comp_val=0.0, show_mode=False, bins=30)
+plt.savefig('Figure_8.8.png')
+
+
+# For Exercise 8.5:
+# Posterior prediction. For each step in the chain, use the posterior thetas 
+# to flip the coins.
+chain_len = len(theta1_sample)
+# Create matrix to hold results of simulated flips:
+y_pred = np.zeros((2, chain_len))
+for step_idx in range(chain_len):  # step through the chain
+    # flip the first coin:
+    p_head1 = theta1_sample[step_idx]
+    y_pred[0, step_idx] = np.random.choice([0,1], p=[1-p_head1, p_head1])
+    # flip the second coin:
+    p_head2 = theta2_sample[step_idx]
+    y_pred[1, step_idx] = np.random.choice([0,1], p=[1-p_head2, p_head2])
+
+
+# Now determine the proportion of times that y1==1 and y2==0
+pY1eq1andY2eq0 = sum((y_pred[0] ==1) & (y_pred[1] == 0)) / chain_len
+
+print pY1eq1andY2eq0
+plt.show()
+
