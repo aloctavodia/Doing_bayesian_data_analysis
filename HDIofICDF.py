c0da6b03f1fefb43a4dff5407e053f8c1e5bf11a
aloctavodia <aloctavodia@gmail.com>
2014-09-19 15:48:50 -0300

    remove intermediate variable add inline comments

--- HDIofICDF.py
+++ HDIofICDF.py
@@ -5,15 +5,17 @@ mathematically in Python.
 from scipy.optimize import fmin
 from scipy.stats import *
 
-
 def HDIofICDF(dist_name, credMass=0.95, **args):
+    # freeze distribution with given arguments
     distri = dist_name(**args)
-    #distri = norm(0, 1)
+    # initial guess for HDIlowTailPr
     incredMass =  1.0 - credMass
+
     def intervalWidth(lowTailPr):
         return distri.ppf(credMass + lowTailPr) - distri.ppf(lowTailPr)
 
-    optInfo = fmin(intervalWidth, incredMass, ftol=1e-8, disp=False)
-    HDIlowTailPr = optInfo
-    return np.array((distri.ppf(HDIlowTailPr)[0], 
-                    distri.ppf(credMass + HDIlowTailPr)[0]))
+    # find lowTailPr that minimizes intervalWidth
+    HDIlowTailPr = fmin(intervalWidth, incredMass, ftol=1e-8, disp=False)[0]
+    # return interval as array([low, high])
+    return distri.ppf([HDIlowTailPr, credMass + HDIlowTailPr])
+
